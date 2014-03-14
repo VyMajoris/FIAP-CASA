b7446a2830d1024fab04e19ab4d6a59a2006a71e
VyMajoris <VyMajoris>
2014-03-14 04:49:17 -0300

    Added - Aula 6 Enfermeira

---
+++ Aula 6 - Enfermeira/Aula 6 - Enfermeira/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Aula 6 - Enfermeira
+//
+//  Created by VyMajoris on 3/14/14.
+//  Copyright (c) 2014 VyMajoris. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
